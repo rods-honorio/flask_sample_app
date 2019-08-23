29ec37211d6fe4142683e42264a26728fd714e6c
Rods Honorio <rods.honorio@gmail.com>
2019-08-23 01:34:48 -0300

    Add error handling pages for 404 and 500

--- sample.py
+++ sample.py
@@ -11,11 +11,12 @@ def create_app():
     from view.app_view import sample_app_bp
     from view.sample_a_view import sample_a_bp
     from view.session_view import session_bp
-    from view.errors import errors_bp
+    from view.error_view import errors_bp, not_found_error
     app.register_blueprint(sample_app_bp, url_prefix='/flask')
     app.register_blueprint(sample_a_bp, url_prefix='/flask')
     app.register_blueprint(session_bp, url_prefix='/flask')
     app.register_blueprint(errors_bp, url_prefix='/flask')
+    app.register_error_handler(404, not_found_error)
 
     return app
 
--- view/error_view.py
+++ view/error_view.py
@@ -6,10 +6,10 @@ errors_bp = Blueprint('errors', __name__)
 
 @errors_bp.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    return render_template('404.html', title='404 Error D:'), 404
 
 
 @errors_bp.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('500.html', title='500 Error D:'), 500
